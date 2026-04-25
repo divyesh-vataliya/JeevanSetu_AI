5aede6fe958a7d75b182d67eb1f45fd9ea50e12c
divyesh-vataliya <divyeshvataliya.ictd23@adaniuni.ac.in>
2026-04-25 15:50:28 +0530

    Enable DEBUG mode temporarily for remote diagnosis

--- jeevansetu/settings.py
+++ jeevansetu/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-_q-i06!g(7qo70vu8(i#-mivxed0qp@fql2=v6ggp0szsz96yw
 import os
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('DEBUG', 'False').lower() == 'true'
+DEBUG = True
 
 ALLOWED_HOSTS = ['jeevansetu-ai.onrender.com', '127.0.0.1', 'localhost', '.onrender.com']
 
